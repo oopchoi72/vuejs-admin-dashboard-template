b7af057d752bb4cbc837c39b6e390a35fe94fe75
oopchoi72 <oopchoi@gmail.com>
2023-12-16 09:09:22 +0900

    404 페이지 추가

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -4,13 +4,11 @@ import ErrorCard from "../partials/errors/ErrorCard.vue";
 
 <template>
   <div className="flex h-screen overflow-hidden">
-    {/* Content area */}
     <div
       className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden"
     >
       <main>
         <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-          {/* Cards */}
           <div className="flex items-center justify-center">
             <ErrorCard
               code="404"
--- src/partials/errors/ErrorCard.vue
+++ src/partials/errors/ErrorCard.vue
@@ -1,2 +1,29 @@
-<script setup></script>
-<template></template>
+<script setup>
+import { defineProps } from "vue";
+import { useRouter } from "vue-router";
+const props = defineProps(["code", "message"]);
+const router = useRouter();
+</script>
+<template>
+  <div className="p-3">
+    <div className="bg-white p-8 rounded-lg shadow-md w-96">
+      <h1 className="text-2xl font-semibold mb-4">에러</h1>
+      <div className="mb-4">
+        <h2>에러 코드 : {{ props.code }}</h2>
+        <p>에러 메시지 : {{ props.message }}</p>
+      </div>
+
+      <button
+        type="submit"
+        className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600"
+        @click.prevent="
+          (event) => {
+            router.push('/');
+          }
+        "
+      >
+        메인 페이지
+      </button>
+    </div>
+  </div>
+</template>
